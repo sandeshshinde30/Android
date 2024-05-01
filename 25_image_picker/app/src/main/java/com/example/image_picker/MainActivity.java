b776e2c7e12243105e2e09552e0ef76c2292efe8
Sandesh Shinde <sandeshshinde7047@gmail.com>
2024-05-02 02:35:51 +0530

    Question 25 on Image Picker from Gallery

---
+++ 25_image_picker/app/src/main/java/com/example/image_picker/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.image_picker;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 100;
+    private ImageView imageView;
+    private Button selectImageBtn;
+    private Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        selectImageBtn = findViewById(R.id.selectImageBtn);
+
+        selectImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setImageURI(imageUri);
+        }
+    }
+}
