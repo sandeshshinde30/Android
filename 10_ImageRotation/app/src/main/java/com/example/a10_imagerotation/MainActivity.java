21dcc6091a36c7d9f9f004f05f85d86bb71e70a3
sharvari-09 <jadhavsharvari9113@gmail.com>
2024-05-02 02:09:26 +0530

    adding 09_RadioButtons, 10_ImageRotation and 12_AutoSearch

---
+++ 10_ImageRotation/app/src/main/java/com/example/a10_imagerotation/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.a10_imagerotation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageButton;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imageButton;
+    private ImageButton rotateButton;
+    private int currentRotation = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButton = findViewById(R.id.imageButton);
+        rotateButton = findViewById(R.id.rotateButton);
+
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotateImage();
+            }
+        });
+    }
+
+    private void rotateImage() {
+        currentRotation += 90;
+        RotateAnimation rotate = new RotateAnimation(currentRotation, currentRotation + 90,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(500);
+        rotate.setFillAfter(true);
+        imageButton.startAnimation(rotate);
+    }
+}
